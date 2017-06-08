88530fcdbd169f2ac13dc6a5c7a1a52fb0e41c13
SMSJOA <tech@smsjoa.com>
2017-06-08 10:32:16 +0900

    Create MessagingService.php

--- curl_example/lib/MessagingService.php
+++ curl_example/lib/MessagingService.php
@@ -36,11 +36,10 @@ class MessagingService
 
 			if($postdata){
 				foreach($postdata as $k => $v){
-					if( $k == "msg" || $k == "subject" ){
-						$v = urlencode($v);
-						$temp[] = $k."=".$v;
-					}
+					if( $k == "msg" || $k == "subject" ) $v = urlencode($v);
+					$temp[] = $k."=".$v;				
 				}
+				
 				$postdata = implode($temp,"&");
 			}
 		}
